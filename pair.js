f7772e093c2dfd4392171e7e51e2d322513f5aad
Nyanuga <juniorezed@gmail.com>
2025-05-22 09:26:36 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Gifted_Tech,
+    default: Prince_Junior_Tech,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function GIFTED_MD_PAIR_CODE() {
+        async function PRINCE_JUNIOR_V2_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+            let Pair_Code_By_Prince_junior_Tech = Prince_Junior_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,16 +35,16 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+             if(!Pair_Code_By_Prince_junior_Tech .authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num)
+                            const code = await Pair_Code_By_Prince_junior_Tech .requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+            Pair_Code_By_Prince_junior_Tech .ev.on('creds.update', saveCreds)
+            Pair_Code_By_Prince_junior_Tech .ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -54,35 +54,35 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Prince_junior_Tech .sendMessage(Pair_Code_By_Prince_junior_Tech .user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
 
 ┏━━━━━━━━━━━━━━
-┃ANONYMOUS-MD SESSION IS 
+┃PRINCE-JUNIOR-V2 SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED 💯🔥
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator =ANONYMOUS POWER 
-      Designed=KING Orman
+❶ || Creator =JUNIOR EZED POWER 
+      Designed=VEE VIVIAN
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y
+❷ || WhattsApp Channel = https://whatsapp.com/channel/0029Vb5U5AI3wtb8r6Gbex2p
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❸ || Owner = https://wa.me/+256784670936
+❸ || Owner = https://wa.me/+25423245807
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❺ || GIT HUB REPO = https://github.com/Terrizev/ANONYMOUS-MD/fork
+❺ || GIT HUB REPO = https://github.com/Nyanuga/Prince-junior-v2
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ Uthuman Dev😌`
- await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
+ await Pair_Code_By_Prince_junior_Tech .sendMessage(Pair_Code_By_Prince_junior_Tech .user.id,{text:PRINCE_JUNIOR_V2_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Gifted_Tech.ws.close();
+        await Pair_Code_By_Prince_junior_Tech .ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    GIFTED_MD_PAIR_CODE();
+                    PRINE_JUNIOR_V2_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -93,6 +93,6 @@ router.get('/', async (req, res) => {
          }
         }
     }
-    return await GIFTED_MD_PAIR_CODE()
+    return await PRINE_JUNIOR_V2_PAIR_CODE()
 });
 module.exports = router
